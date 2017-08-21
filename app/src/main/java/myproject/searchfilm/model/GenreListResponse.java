205fc10490c411b9502834740a14089c04e6fa2d
Aliaksandr <aliaksandr.danilchanka@gmail.com>
2017-08-21 18:05:02 +0300

    Add new model for search genre.

---
+++ app/src/main/java/myproject/searchfilm/model/GenreListResponse.java
@@ -0,0 +1,21 @@
+package myproject.searchfilm.model;
+
+import java.util.List;
+
+/**
+ * Created by Aliaksandr on 8/18/2017.
+ */
+
+public class GenreListResponse {
+
+    private List<Genre> genres;
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+}
